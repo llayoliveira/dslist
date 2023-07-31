81363e3a572fdda6fc0ecee06d7e42d90c01a308
Layane Oliveira <layane2110@hotmail.com>
2023-07-31 13:18:27 -0300

    Get game by id

---
+++ src/main/java/com/devsuperior/dslist/dto/GameDTO.java
@@ -0,0 +1,33 @@
+package com.devsuperior.dslist.dto;
+
+import org.springframework.beans.BeanUtils;
+
+import com.devsuperior.dslist.entities.Game;
+
+import lombok.Getter;
+import lombok.Setter;
+
+
+@Getter
+@Setter
+public class GameDTO {
+	private Long id;
+	private String title;
+	private Integer year;
+	private String genre;
+	private String platforms;
+	private Double score;
+	private String imgUrl;
+	private String shortDescription;
+	private String longDescription;
+	
+	public GameDTO() {
+		
+	}
+	
+	public GameDTO(Game entity) {
+		BeanUtils.copyProperties(entity, this);
+	}
+}
+
+
